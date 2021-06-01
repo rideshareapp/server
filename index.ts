687e2e6efa7a4d74117298519d335c950c30dff9
Nathan Tang <nathan.ly.tang@gmail.com>
2021-06-01 03:08:17 -0600

    Import routes

--- index.ts
+++ index.ts
@@ -1,15 +1,24 @@
 import express from "express";
 import cors from "cors";
-import { query } from "./db/database";
+import { router as users } from "./routes/users";
 
 const app = express();
+
 app.use(cors());
+app.use(express.json());
 
-// async function main() {
-//     const res = await query("SELECT * FROM data", [])
-//     console.log(res.rows)
-// }
+// Routes
+app.use('/users', users);
 
-// main();
+app.get('*', (req: express.Request, res: express.Response) => {
+  res.status(404).json({ message: 'not found' });
+});
+
+// app.use((req, res, next) => {
+//   if (err.type === 'entity.parse.failed') {
+//     return res.status(400).json({ message: 'bad request' });
+//   }
+//   next(err);
+// });
 
 module.exports = app;
