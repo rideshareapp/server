757ae1164989349e6d4e7754439f4dec6036da7f
Nathan Tang <nathan.ly.tang@gmail.com>
2021-06-02 03:51:41 -0600

    Feat: user controllers

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,8 +1,30 @@
-import { query } from "../db/database";
+import * as db from "../db/database";
 
-async function main() {
-    const res = await query("SELECT * FROM data", [])
-    console.log(res.rows)
+export async function orgRegister(): Promise<void> {
+    // Organization registration logic
+    // Add organization to database
+    await db.query("SELECT * FROM data", []);
+    console.log("Organization registration")
 }
 
-main();
+export async function userRegister(): Promise<void> {
+    // User registration logic
+    // Add user to database
+    await db.query("SELECT * FROM data", []);
+    console.log("User registration")
+}
+
+export async function login(): Promise<void> {
+    // User login logic
+    // Authentication
+    // Store session tokens
+    await db.query("SELECT * FROM data", []);
+    console.log("User login")
+}
+
+export async function logout(): Promise<void> {
+    // User logout logic
+    // Delete session and tokens
+    await db.query("SELECT * FROM data", []);
+    console.log("User logout")
+}
--- routes/users.ts
+++ routes/users.ts
@@ -1,16 +1,21 @@
 import express from 'express';
 const router = express.Router();
+import * as controller from "../controllers/users";
 
-router.get('/register', function(req, res) {
+
+router.get('/register', async function(req, res) {
+    await controller.userRegister();
     res.status(200).json({ message: 'User registration here' });
 });
 
-router.get('/login', function(req, res) {
+router.get('/login', async function(req, res) {
+    await controller.login();
     res.status(200).json({ message: 'User login here' });
 });
 
-router.get('/logout', function(req, res) {
+router.get('/logout', async function(req, res) {
+    await controller.logout();
     res.status(200).json({ message: 'User logout here' });
 });
 
-export { router }
+export { router };
