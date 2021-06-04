956b1073cac9dda73e679840b9c0d61ce99458bc
Nathan Tang <nathan.ly.tang@gmail.com>
2021-06-04 03:15:37 -0600

    Change routes to post

--- routes/users.ts
+++ routes/users.ts
@@ -1,20 +1,22 @@
 import express from 'express';
 const router = express.Router();
-import * as controller from "../controllers/users";
+import * as userService from "../controllers/users";
 
 
-router.get('/register', async function(req, res) {
-    await controller.userRegister();
-    res.status(200).json({ message: 'User registration here' });
+router.post('/register', async function(req, res) {
+    console.log(req.body);
+    const details = await userService.userRegister(req.body);
+    res.status(200).json({ message: 'User registration here', details: details });
 });
 
-router.get('/login', async function(req, res) {
-    await controller.login();
-    res.status(200).json({ message: 'User login here' });
+router.post('/login', async function(req, res) {
+    console.log(req.body);
+    const details = await userService.login(req.body.email, req.body.password);
+    res.status(200).json({ message: 'User login here', details: details });
 });
 
-router.get('/logout', async function(req, res) {
-    await controller.logout();
+router.post('/logout', async function(req, res) {
+    await userService.logout();
     res.status(200).json({ message: 'User logout here' });
 });
 
