2c82d7628ebd05cb5ba88acb5c22fa3639858349
Nathan Tang <nathan.ly.tang@gmail.com>
2021-06-04 23:15:37 -0600

    Fix: all functions return correct object on query

--- db/utils.ts
+++ db/utils.ts
@@ -1,27 +1,54 @@
 import * as db from "./database";
 import * as userModel from "../models/users";
 
-export async function createUser(user: userModel.User): Promise<number> {
-    await db.query("INSERT INTO users VALUES($1, $2, $3, $4, $5)", [user.email, user.first, user.last, user.phone, user.password]);
-    return 200;
+/**
+ * Create a new user and add to database
+ * 
+ * @param user - User model
+ * @returns true or false
+ */
+export async function createUser(user: userModel.User): Promise<boolean> {
+    try {
+        const params = [user.email, user.first, user.last, user.phone, user.password];
+        await db.query("INSERT INTO users VALUES($1, $2, $3, $4, $5)", params);
+        return true;
+    } catch {
+        return false;
+    }
 }
 
-export async function findUser(user: string): Promise<Array<unknown>> {
-    const res = await db.query("SELECT * FROM users WHERE email = $1", [user]);
-    return res.rows[0];
+/**
+ * Get all user data if user exists in database
+ * 
+ * @param user - User email
+ * @returns User model
+ */
+export async function findUser(email: string): Promise<userModel.User> {
+    const res = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+    const userArray = await res.rows[0];
+    const user = new userModel.User(userArray.first_name, userArray.last_name, userArray.phone, userArray.email, userArray.pw_hashed);
+    return user;
+    // return (await db.query("SELECT * FROM users WHERE email = $1", [user])).rows[0];
 }
 
-export async function checkUserExists(user: string): Promise<boolean> {
-    const res = await db.query("SELECT * FROM users WHERE email = $1", [user]);
-    // if (res.rows.length === 0) {
-    //     return false;
-    // } else {
-    //     return true;
-    // }
+/**
+ * Checks if a user email is already in the database
+ * 
+ * @param user - User email
+ * @returns true or false
+ */
+export async function checkUserExists(email: string): Promise<boolean> {
+    const res = await db.query("SELECT * FROM users WHERE email = $1", [email]);
     return (res.rows.length === 0 ? false : true);
 }
 
-export async function getPassword(user: string): Promise<string> {
-    const res = await db.query("SELECT pw_hashed FROM users WHERE email = $1", [user]);
-    return res.rows[0][0];
+/**
+ * Get the password of a user from the users database
+ * 
+ * @param user - User email
+ * @returns hashed password of user
+ */
+export async function getPassword(email: string): Promise<string> {
+    const res = await db.query("SELECT pw_hashed FROM users WHERE email = $1", [email]);
+    return res.rows[0].pw_hashed;
 }
