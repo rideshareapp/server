348f2a6cdf990165d44d5f8f1323b66a01123ee5
Nathan Tang <nathan.ly.tang@gmail.com>
2021-06-04 23:16:53 -0600

    Add url encoded middleware

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded());
 
 // Routes
 router(app);
@@ -14,11 +15,12 @@ app.get('*', (req: express.Request, res: express.Response) => {
   res.status(404).json({ message: 'not found' });
 });
 
-// app.use((req, res, next) => {
-//   if (err.type === 'entity.parse.failed') {
-//     return res.status(400).json({ message: 'bad request' });
+// app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+//   if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+//       console.error(err);
+//       return res.sendStatus(400); // Bad request
 //   }
-//   next(err);
+//   next();
 // });
 
 module.exports = app;
