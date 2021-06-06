c54dab7f9ee1eecaf74e4cb4c8dd2ee031b28a12
Nathan <nathan.ly.tang@gmail.com>
2021-06-05 23:49:59 -0600

    Ref: rename userService to userController

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 router(app);
--- routes/users.ts
+++ routes/users.ts
@@ -1,20 +1,20 @@
 import express from 'express';
 const router = express.Router();
-import * as userService from "../controllers/users";
+import * as userController from "../controllers/users";
 
 
 router.post('/register', async function(req, res) {
-    const details = await userService.userRegister(req.body);
+    const details = await userController.userRegister(req.body);
     res.status(200).json({ message: 'User registration here', details: details });
 });
 
 router.post('/login', async function(req, res) {
-    const details = await userService.login(req.body.email, req.body.password);
+    const details = await userController.login(req.body.email, req.body.password);
     res.status(200).json({ message: 'User login here', details: details });
 });
 
 router.post('/logout', async function(req, res) {
-    await userService.logout();
+    await userController.logout();
     res.status(200).json({ message: 'User logout here' });
 });
 
