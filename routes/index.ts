f92724643ec701098cdbf7421e3f06f3eee962f4
Nathan <nathan.ly.tang@gmail.com>
2021-06-09 13:25:54 -0600

    Add org route

--- routes/index.ts
+++ routes/index.ts
@@ -1,6 +1,8 @@
 import { Application } from "express";
 import { router as users } from "./users";
+import { router as organizations } from "./organizations";
 
 export default function(app: Application): void {
     app.use('/users', users);
+    app.use('/organizations', organizations);
 }
