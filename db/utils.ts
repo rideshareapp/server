773c3026229ba65ae7f032582cb739e0904e7d40
Nathan <nathan.ly.tang@gmail.com>
2021-06-09 13:25:05 -0600

    Feat: Org specific queries, globalize password query

--- db/utils.ts
+++ db/utils.ts
@@ -1,9 +1,47 @@
 import * as db from "./database";
 import * as userModel from "../models/users";
+import * as orgModel from "../models/organization";
+
+
+/**
+ * Create a new organization and add to database
+ * @param org Organization model
+ * @returns true or false
+ */
+export async function createOrg(org: orgModel.Organization): Promise<boolean> {
+    try {
+        const params = [org.name, org.code, org.email, org.password];
+        await db.query("INSERT INTO organizations VALUES($1, $2, $3, $4)", params);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+/**
+ * Gets organization data if organization exists in database
+ * @param email Organization email
+ * @returns Organization model
+ */
+export async function findOrg(email: string): Promise<orgModel.Organization> {
+    const res = await db.query("SELECT * FROM organizations WHERE email = $1", [email]);
+    const orgArray = await res.rows[0];
+    const org = new orgModel.Organization(orgArray.name, orgArray.code, orgArray.email, orgArray.password);
+    return org;
+}
+
+/**
+ * Checks if an organization email is already in the database
+ * @param email - User email
+ * @returns true or false
+ */
+ export async function checkOrgExists(email: string): Promise<boolean> {
+    const res = await db.query("SELECT * FROM organizations WHERE email = $1", [email]);
+    return (res.rows.length === 0 ? false : true);
+}
 
 /**
  * Create a new user and add to database
- * 
  * @param user - User model
  * @returns true or false
  */
@@ -19,7 +57,6 @@ export async function createUser(user: userModel.User): Promise<boolean> {
 
 /**
  * Get all user data if user exists in database
- * 
  * @param user - User email
  * @returns User model
  */
@@ -33,22 +70,22 @@ export async function findUser(email: string): Promise<userModel.User> {
 
 /**
  * Checks if a user email is already in the database
- * 
- * @param user - User email
+ * @param email - User email
  * @returns true or false
  */
-export async function checkUserExists(email: string): Promise<boolean> {
-    const res = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+ export async function checkEntityExists(email: string, type: "users" | "organizations"): Promise<boolean> {
+    const query = `SELECT * FROM ${type} WHERE email = $1`;
+    const res = await db.query(query, [email]);
     return (res.rows.length === 0 ? false : true);
 }
 
 /**
  * Get the password of a user from the users database
- * 
  * @param user - User email
  * @returns hashed password of user
  */
-export async function getPassword(email: string): Promise<string> {
-    const res = await db.query("SELECT pw_hashed FROM users WHERE email = $1", [email]);
+export async function getPassword(email: string, type: "users" | "organizations"): Promise<string> {
+    const query = `SELECT pw_hashed FROM ${type} WHERE email = $1`;
+    const res = await db.query(query, [email]);
     return res.rows[0].pw_hashed;
 }
