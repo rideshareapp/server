19c12fc8c52301a43689905ffafbc42a8b717b45
Nathan <nathan.ly.tang@gmail.com>
2021-06-09 13:25:42 -0600

    Returns token on successful login

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,49 +1,44 @@
 import * as db from "../db/utils";
 import * as userModel from "../models/users";
-import * as userService from "../services/users";
-
-export async function orgRegister(): Promise<void> {
-    // Organization registration logic
-    // Add organization to database
-    console.log("Organization registration");
-}
+import * as services from "../services/";
 
 export async function userRegister(user: userModel.User): Promise<unknown> {
     // User registration logic
 
     // User already exists
-    if (await db.checkUserExists(user.email)) {
+    if (await db.checkEntityExists(user.email, "users")) {
         return "user already exists";
     }
 
     // Create new user
-    userService.createNewUser(user);
-    
-    console.log("User registration");
-    return "User created";
+    if (!await services.userService.createNewUser(user)) {
+        return "failed to create new user";
+    }
+
+    // User login
+    const token = login(user.email, user.password);
+    return token;
 }
 
 export async function login(email: string, password: string): Promise<unknown> {
     // User login logic
     // Authentication
     // Store session tokens
-    if (!await db.checkUserExists(email)) {
+    if (!await db.checkEntityExists(email, "users")) {
         return "user not found";
     }
     // Authenticate user
-    if (!await userService.authenticateUser(email, password)) {
+    if (!await services.authenticateAcc(email, password, "users")) {
         return "password mismatch";
     }
 
+
     // Return token
-    const token = await userService.tokenizeUser(email);
-    const user = await db.findUser(email);
-    console.log("User login");
+    const token = await services.tokenizeAcc(email);
     return token;
 }
 
 export async function logout(): Promise<void> {
     // User logout logic
     // Delete session and tokens
-    console.log("User logout");
 }
