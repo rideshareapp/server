0588bde98aa07c6fec280f45bf11a2195346291f
Nathan <nathan.ly.tang@gmail.com>
2021-06-17 15:27:43 -0700

    Init: get events route

--- routes/events.ts
+++ routes/events.ts
@@ -2,16 +2,20 @@
 
 import express from 'express';
 const router = express.Router();
-import * as orgController from "../controllers/organizations";
+import * as eventController from "../controllers/events";
 
 
 router.post('/create', async function (req, res) {
-    const created = await orgController.createEvent(req.body);
+    const created = await eventController.createEvent(req.body);
     if (created) {
         res.status(201).json({ message: 'Create event here', details: created });
     } else {
         res.status(400).json({ message: 'Create event here', details: created });
     }
+});
+
+router.get('/getAll', async function (req, res) {
+    // 
 });
 
 export { router };
