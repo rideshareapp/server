6579a97be0873f176e9da96969e5500b9ee8f2b2
Nathan <nathan.ly.tang@gmail.com>
2021-06-20 17:17:52 -0600

    Ref: Move respoinse data to controllers

--- routes/users.ts
+++ routes/users.ts
@@ -4,28 +4,12 @@ import express from 'express';
 const router = express.Router();
 import * as userController from "../controllers/users";
 
+// Authentication routes
+router.post('/register', userController.userRegister);
+router.post('/login', userController.login);
+router.post('/logout', userController.logout);
 
-router.post('/register', async function (req, res) {
-    const details = await userController.userRegister(req.body);
-    res.status(200).json({ message: 'User registration here', details: details });
-});
-
-router.post('/login', async function (req, res) {
-    const details = await userController.login(req.body.email, req.body.password);
-    res.status(200).json({ message: 'User login here', details: details });
-});
-
-router.post('/logout', async function (req, res) {
-    await userController.logout();
-    res.status(200).json({ message: 'User logout here' });
-});
-
-router.put('/joinOrg', async function (req, res) {
-    if (await userController.joinOrg(req.body.code, req.body.email)) {
-        res.status(201).json({ message: 'Org joined' });
-    } else {
-        res.status(400).json({ message: 'Failed to join org' });
-    }
-});
+// Organization related routes
+router.put('/joinOrg', userController.joinOrg);
 
 export { router };
