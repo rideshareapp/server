27a83358b7c4590c625ca947c47c869d132617b5
Nathan <nathan.ly.tang@gmail.com>
2021-06-20 17:18:44 -0600

    Feat: functions now return responses directly

--- controllers/organizations.ts
+++ controllers/organizations.ts
@@ -4,38 +4,86 @@ import * as db from "../db/utils";
 import * as orgModel from "../models/organization";
 import * as services from "../services";
 import * as auth from "../auth";
+import { Request, Response, NextFunction } from "express";
+import { Success, Error } from "../models/return_status";
 
-export async function orgRegister(org: orgModel.Organization): Promise<unknown> {
-    // Organization registration logic
+// export async function orgRegister(org: orgModel.Organization): Promise<unknown> {
+//     // Organization registration logic
 
-    // Check if org already exists
-    if (await db.checkEntityExists(org.email, "organizations")) {
-        return "org already exists";
+//     // Check if org already exists
+//     if (await db.checkEntityExists(org.email, "organizations")) {
+//         return "org already exists";
+//     }
+//     // Create new organization
+//     // if (!await services.orgService.createNewOrg(org)) {
+//     //     return "failed to create new org";
+//     // }
+//     try {
+//         await services.orgService.createNewOrg(org);
+//     } catch (err) {
+//         return ["error", "failed to create new org"];
+//     }
+
+//     // User login
+//     const token = login(org.email, org.password);
+//     return token;
+// }
+export async function orgRegister(req: Request, res: Response, next: NextFunction): Promise<unknown> {
+    try {
+        // Organization registration logic
+        const org: orgModel.Organization = req.body;
+        if (await db.checkEntityExists(org.email, "organizations")) {
+            return res.status(409).json(new Error("org already exists"));
+        }
+        if (!await services.orgService.createNewOrg(org)) {
+            return res.status(500).json(new Error("failed to create org"));
+        }
+        return await login(req, res, next);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json(new Error("internal server error"));
     }
-    // Create new organization
-    if (!await services.orgService.createNewOrg(org)) {
-        return "failed to create new org";
-    }
-    // User login
-    const token = login(org.email, org.password);
-    return token;
+
 }
 
-export async function login(email: string, password: string): Promise<unknown> {
-    // Organization login logic
-    // Authentication
-    // Store session tokens
-    if (!await db.checkEntityExists(email, "organizations")) {
-        return "user not found";
+export async function login(req: Request, res: Response, next: NextFunction): Promise<unknown> {
+    try {
+        // TODO: Store session tokens
+
+        // Authenticate organization login
+        if (!await db.checkEntityExists(req.body.email, "organizations")) {
+            return res.status(409).json(new Error("org not found"));
+        }
+        if (!await auth.authenticateAcc(req.body.email, req.body.password, "organizations")) {
+            return res.status(401).json(new Error("password mismatch"));
+        }
+        // Return token
+        const token = await auth.tokenizeAcc(req.body.email);
+        return res.status(200).json(new Success({ "token": token }));
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json(new Error("internal server error"));
     }
-    // Authenticate user
-    if (!await auth.authenticateAcc(email, password, "organizations")) {
-        return "password mismatch";
-    }
-    // Return token
-    const token = await auth.tokenizeAcc(email);
-    return token;
 }
+
+
+// try {
+//     // TODO: Store session tokens
+
+//     // Authenticate user
+//     if (!await db.checkEntityExists(req.body.email, "users")) {
+//         return res.status(409).json(new Error("user not found"));
+//     }
+//     if (!await auth.authenticateAcc(req.body.email, req.body.password, "users")) {
+//         return res.status(401).json(new Error("password mismatch"));
+//     }
+//     // Return token
+//     const token = await auth.tokenizeAcc(req.body.email);
+//     return res.status(200).json(new Success({ "token": token }));
+// } catch (err) {
+//     console.error(err);
+//     return res.status(500).json(new Error("internal server error"));
+// }
 
 export async function logout(): Promise<void> {
     // User logout logic
--- controllers/users.ts
+++ controllers/users.ts
@@ -2,42 +2,45 @@
 
 import * as db from "../db/utils";
 import * as userModel from "../models/users";
+import { Success, Error } from "../models/return_status";
 import * as services from "../services/";
 import * as auth from "../auth";
+import { Request, Response, NextFunction } from "express";
 
-export async function userRegister(user: userModel.User): Promise<unknown> {
-    // User registration logic
-
-    // User already exists
-    if (await db.checkEntityExists(user.email, "users")) {
-        return "user already exists";
+export async function userRegister(req: Request, res: Response, next: NextFunction): Promise<Response> {
+    try {
+        const user: userModel.User = req.body; // Create a new user from body content
+        if (await db.checkEntityExists(req.body.email, "users")) {
+            return res.status(409).json(new Error("user already exists"));
+        }
+        if (!await services.userService.createNewUser(user)) {
+            return res.status(500).json(new Error("failed to create user"));
+        }
+        return await login(req, res, next);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json(new Error("internal server error"));
     }
-
-    // Create new user
-    if (!await services.userService.createNewUser(user)) {
-        return "failed to create new user";
-    }
-
-    // User login
-    const token = login(user.email, user.password);
-    return token;
 }
 
-export async function login(email: string, password: string): Promise<unknown> {
-    // User login logic
-    // Authentication
-    // Store session tokens
-    if (!await db.checkEntityExists(email, "users")) {
-        return "user not found";
-    }
-    // Authenticate user
-    if (!await auth.authenticateAcc(email, password, "users")) {
-        return "password mismatch";
-    }
+export async function login(req: Request, res: Response, next: NextFunction): Promise<Response> {
+    try {
+        // TODO: Store session tokens
 
-    // Return token
-    const token = await auth.tokenizeAcc(email);
-    return token;
+        // Authenticate user
+        if (!await db.checkEntityExists(req.body.email, "users")) {
+            return res.status(409).json(new Error("user not found"));
+        }
+        if (!await auth.authenticateAcc(req.body.email, req.body.password, "users")) {
+            return res.status(401).json(new Error("password mismatch"));
+        }
+        // Return token
+        const token = await auth.tokenizeAcc(req.body.email);
+        return res.status(200).json(new Success({ "token": token }));
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json(new Error("internal server error"));
+    }
 }
 
 export async function logout(): Promise<void> {
@@ -45,16 +48,20 @@ export async function logout(): Promise<void> {
     // Delete session and tokens
 }
 
-/**
- * Add organization code to user list
- * @param code Organization code
- * @param email User email
- * @returns 
- */
- export async function joinOrg(code: string, email: string): Promise<boolean> {
-    //  TODO: Check if org code exists
-    if (!await db.checkOrgExists(code, "org_code")) {
-        return false;
+export async function joinOrg(req: Request, res: Response, next: NextFunction): Promise<Response> {
+    try {
+        if (!await db.checkOrgExists(req.body.code, "org_code")) {
+            return res.status(409).json(new Error("org does not exist"));
+        }
+        if (await db.checkUserInOrg(req.body.code, req.body.email)) {
+            return res.status(409).json(new Error("user already in org"));
+        }
+        if (!await db.joinOrg(req.body.code, req.body.email)) {
+            return res.status(500).json(new Error("failed to join org"));
+        }
+        return res.status(201).json(new Success("joined org"));
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json(new Error("internal server error"));
     }
-    return (await db.joinOrg(code, email) ? true : false);
 }
