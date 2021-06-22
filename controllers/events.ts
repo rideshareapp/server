5838792ca6a4b36bc13d52d39044a001a0d6591f
Nathan Tang <nathan.ly.tang@gmail.com>
2021-06-22 16:19:17 -0600

    Event controller returns http reponses directly

--- controllers/events.ts
+++ controllers/events.ts
@@ -1,23 +1,33 @@
 // Controllers: Events
 
 import * as services from "../services";
+import { Success, Error } from "../models/return_status";
+import { Request, Response, NextFunction } from "express";
 
-export async function createEvent(req: eventRequest): Promise<unknown> {
+export async function createEvent(req: Request, res: Response, next: NextFunction): Promise<unknown> {
     try {
-        return (await services.eventService.createEvent(req.code, req.name, req.date, req.include_time) ? true : false);
+        // return (await services.eventService.createEvent(req.code, req.name, req.date, req.include_time) ? true : false);
+        const event: eventRequest = req.body;
+        if (!await services.eventService.createEvent(event.code, event.name, event.date, event.include_time)) {
+            return res.status(400).json(new Error("failed to create event"));
+        }
+        return res.status(201).json(new Success("event created"));
     } catch (err) {
         console.log(err);
-        return false;
+        return res.status(500).json(new Error("internal server error"));
     }
 }
 
-export async function getEvents(req: eventCode): Promise<unknown> {
+export async function getEvents(req: Request, res: Response, next: NextFunction): Promise<unknown> {
     try {
-        const eventList = await services.eventService.getEvents(req.code);
-        return eventList;
-    } catch(err) {
+        const eventList = await services.eventService.getEvents(req.body.code);
+        if (!eventList) {
+            return res.status(400).json(new Error("failed to get events"));
+        }
+        return res.status(200).json(new Success({ eventList }));
+    } catch (err) {
         console.log(err);
-        return err;
+        return res.status(500).json(new Error("internal server error"));
     }
 }
 
@@ -26,8 +36,4 @@ interface eventRequest {
     name: string;
     date: Date;
     include_time: boolean;
-}
-
-interface eventCode {
-    code: string;
 }
--- routes/events.ts
+++ routes/events.ts
@@ -5,18 +5,7 @@ const router = express.Router();
 import * as eventController from "../controllers/events";
 
 
-router.post('/create', async function (req, res) {
-    const created = await eventController.createEvent(req.body);
-    if (created) {
-        res.status(201).json({ message: 'Create event here', details: created });
-    } else {
-        res.status(400).json({ message: 'Create event here', details: created });
-    }
-});
-
-router.get('/getAll', async function (req, res) {
-    const eventList = await eventController.getEvents(req.body);
-    res.status(200).json({ message: 'Get events', details: eventList });
-});
+router.post('/create', eventController.createEvent);
+router.get('/getAll', eventController.getEvents);
 
 export { router };
