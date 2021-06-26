cc49b8e0991e87f4640b16b1c3a7d3edab364923
Nathan Tang <nathan.ly.tang@gmail.com>
2021-06-26 16:19:50 -0600

    Generate a 4 digit alphanumeric code

--- services/organizations.ts
+++ services/organizations.ts
@@ -11,6 +11,14 @@ import * as auth from "../auth";
  */
 export async function createNewOrg(org: orgModel.Organization): Promise<unknown> {
     try {
+        // Generate a code and check for conflicts in database
+        let codeExists = true;
+        while (codeExists) {
+            org.code = await generateCode();
+            if (db.checkOrgCodeExists(org.code)) { // If the code does not already exist then continue
+                codeExists = false;
+            }
+        }
         const newOrg = new orgModel.Organization(org.name, org.code, org.email, org.password);
         newOrg.password = await auth.hashPassword(newOrg.password);
         return (await db.createOrg(newOrg) ? true : false);
@@ -18,4 +26,15 @@ export async function createNewOrg(org: orgModel.Organization): Promise<unknown>
         console.error(err);
         return false;
     }
+}
+
+export async function generateCode(): Promise<string> {
+    const len = 4;
+    const charSet = 'ab0cd1ef2gh3ij4kl5mn6op7qr8st9uvwxyz';
+    let org_code = '';
+    for (let i = 0; i < len; i++) {
+        const randomPoz = Math.floor(Math.random() * charSet.length);
+        org_code += charSet.substring(randomPoz, randomPoz + 1);
+    }
+    return org_code;
 }
