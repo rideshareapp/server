938c8fd748ec3dacbf284a12ee92083bc0293eb6
Nathan Tang <nathan.ly.tang@gmail.com>
2021-07-02 16:57:20 -0600

    Requires helmet

--- index.ts
+++ index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes";
+import helmet from "helmet";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+// app.disable("x-powered-by");
 
 // Routes
 router(app);
