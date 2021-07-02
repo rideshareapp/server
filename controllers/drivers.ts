ada296a4c0379d039ad07545711598c5e25b6f87
Nathan Tang <nathan.ly.tang@gmail.com>
2021-07-02 16:58:53 -0600

    Feat: view and accept trips/requests

--- controllers/drivers.ts
+++ controllers/drivers.ts
@@ -1,29 +1,16 @@
 // Controllers: Drivers
 
-import * as services from "../services";
 import * as db from "../db/utils";
 import { Success, Error } from "../models/return_status";
 import { Request, Response } from "express";
 
 export async function newDriver(req: Request, res: Response): Promise<Response> {
     try {
-        // if (!await db.newDriver(req.body.email, req.body.code)) {
-        //     return res.status(500).json(new Error("internal server error"));
-        // }
+        if (await db.getOrgCode(req.user.email) !== req.body.code) {
+            return res.status(403);
+        }
         await db.newDriver(req.body.email, req.body.code);
         return res.status(201).json(new Success("driver added"));
-    } catch (err) {
-        console.error(err);
-        return res.status(500).json(new Error("internal server error"));
-    }
-}
-
-export async function acceptTrip(req: Request, res: Response): Promise<Response> {
-    try {
-        if (!db.acceptTripRequest(req.body.event_id, req.body.trip_request_id, req.body.driver)) {
-            return res.status(500).json(new Error("internal server error"));
-        }
-        return res.status(201).json(new Success("trip accepted"));
     } catch (err) {
         console.error(err);
         return res.status(500).json(new Error("internal server error"));
--- controllers/trips.ts
+++ controllers/trips.ts
@@ -1,13 +1,29 @@
 // Controllers: Trips
 
-import * as services from "../services";
 import * as db from "../db/utils";
 import { Success, Error } from "../models/return_status";
 import { Request, Response } from "express";
 
-export async function getAll(req: Request, res: Response): Promise<Response> {
+// Drivers
+export async function acceptTrip(req: Request, res: Response): Promise<Response> {
     try {
-        const tripRequests = await db.getTripRequests(req.body.event_id);
+        if (!db.acceptTripRequest(req.body.trip_request_id, req.user.email)) {
+            return res.status(500).json(new Error("internal server error"));
+        }
+        return res.status(201).json(new Success("trip accepted"));
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json(new Error("internal server error"));
+    }
+}
+
+export async function getTripRequestsDriver(req: Request, res: Response): Promise<Response> {
+    try {
+        // Check if driver is allowed to view requests for an event
+        if (!await db.driverInOrg(req.user.email, req.body.event_id)) {
+            return res.status(403);
+        }
+        const tripRequests = await db.getTripRequestsDriver(req.body.event_id);
         return res.status(200).json(new Success(tripRequests));
     } catch (err) {
         console.error(err);
@@ -15,12 +31,56 @@ export async function getAll(req: Request, res: Response): Promise<Response> {
     }
 }
 
-export async function newRequest(req: Request, res:Response): Promise<Response> {
+export async function getConfirmedTripsDriver(req: Request, res: Response): Promise<Response> {
     try {
-        if (!await db.newTripRequest(req.body.event_id, req.body.email, req.body.geolocation, req.body.passengers)) {
+        const trips = await db.getConfirmedTripsDriver(req.user.email);
+        return res.status(200).json(new Success(trips));
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json(new Error("internal server error"));
+    }
+}
+
+export async function finishTrip(req: Request, res: Response): Promise<Response> {
+    try {
+        if (!await db.deleteTrip(req.user.email, req.body.event_id)) {
+            return res.status(500).json(new Error("internal server error"));
+        }
+        return res.status(201).json(new Success("trip finished"));
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json(new Error("internal server error"));
+    }
+}
+
+
+// Users
+export async function newTripRequest(req: Request, res: Response): Promise<Response> {
+    try {
+        if (!await db.newTripRequest(req.body.event_id, req.user.email, req.body.geolocation, req.body.passengers_num)) {
             return res.status(500).json(new Error("internal server error"));
         }
         return res.status(201).json(new Success("created new request"));
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json(new Error("internal server error"));
+    }
+}
+
+export async function getTripRequestsUser(req: Request, res: Response): Promise<Response> {
+    try {
+        const tripRequests = await db.getTripRequestsUser(req.user.email);
+        return res.status(200).json(new Success(tripRequests));
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json(new Error("internal server error"));
+    }
+}
+
+export async function getConfirmedTripsUser(req: Request, res: Response): Promise<Response> {
+    try {
+        const trips = await db.getConfirmedTripsUser(req.user.email);
+        return res.status(200).json(new Success(trips));
     } catch (err) {
         console.error(err);
         return res.status(500).json(new Error("internal server error"));
