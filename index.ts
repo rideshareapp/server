e90c7c8a2721a82a4a67434271a4f2527243a38b
Nathan Tang <nathan.ly.tang@gmail.com>
2021-07-05 03:06:45 -0600

    Update origin in CORS

--- index.ts
+++ index.ts
@@ -2,13 +2,15 @@ import express from "express";
 import cors from "cors";
 import router from "./routes";
 import helmet from "helmet";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({credentials: true, origin: process.env.ORIGIN}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
+app.use(cookieParser());
 // app.disable("x-powered-by");
 
 // Routes
