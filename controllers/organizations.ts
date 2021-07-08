19d844108cc9476352d53fe72a1d6730a830b150
Nathan Tang <nathan.ly.tang@gmail.com>
2021-07-08 03:16:18 -0600

    Return refresh toekn on login, implement logout

--- controllers/organizations.ts
+++ controllers/organizations.ts
@@ -1,6 +1,7 @@
 // Controllers: Organizations
 
 import * as db from "../db/utils";
+import * as redis from "../db/redis";
 import * as orgModel from "../models/organization";
 import * as services from "../services";
 import * as auth from "../auth";
@@ -37,9 +38,15 @@ export async function login(req: Request, res: Response): Promise<Response> {
         if (!await auth.authenticateAcc(req.body.email, req.body.password, "organizations")) {
             return res.status(401).json(new Error("password mismatch"));
         }
-        // Return token
+
+        // Create access and refresh token
         const access_token = await auth.createAccessToken(req.body.email);
         const refresh_token = await auth.createRefreshToken(req.body.email);
+
+        // Insert refresh token into Redis database
+        redis.insertKey(refresh_token, req.body.email);
+
+        // Return token
         res.cookie('ACCESS_TOKEN', access_token, { httpOnly: true, expires: new Date(new Date().getTime() + 1000 * parseInt(process.env.ACCESS_TOKEN_EXPIRY || "")), sameSite: "strict" });
         res.cookie('REFRESH_TOKEN', refresh_token, { httpOnly: true, expires: new Date(new Date().getTime() + 1000 * parseInt(process.env.ACCESS_TOKEN_EXPIRY || "")), path: "/token/refresh", sameSite: "strict" });
         return res.status(200).json(new Success({ "access_token": access_token, "refresh_token": refresh_token }));
@@ -49,7 +56,13 @@ export async function login(req: Request, res: Response): Promise<Response> {
     }
 }
 
-export async function logout(): Promise<void> {
+export async function logout(req: Request, res: Response): Promise<Response> {
     // User logout logic
-    // Delete session and tokens
+    try {
+        redis.deleteKey(req.cookies.REFRESH_TOKEN);
+        return res.sendStatus(401);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json(new Error("internal server error"));
+    }
 }
