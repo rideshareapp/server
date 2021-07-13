4eecfd00f458a2d8895e29600769960747ce9e9f
Nathan Tang <nathan.ly.tang@gmail.com>
2021-07-12 23:05:42 -0600

    Ref: Put list of orgs and users in a separate table

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -6,21 +6,26 @@ BEGIN
         first_name VARCHAR(255) NOT NULL,
         last_name VARCHAR(255) NOT NULL,
         phone VARCHAR(50) NOT NULL,
-        pw_hashed VARCHAR(72) NOT NULL,
-        in_orgs text[]
+        pw_hashed VARCHAR(72) NOT NULL
     );
 
     CREATE TABLE IF NOT EXISTS drivers (
         email VARCHAR(255) NOT NULL REFERENCES users(email) ON DELETE CASCADE ON UPDATE CASCADE,
-        org_code CHAR(4) NOT NULL REFERENCES organizations(org_code) ON DELETE CASCADE ON UPDATE CASCADE,
+        org_code CITEXT NOT NULL REFERENCES organizations(org_code) ON DELETE CASCADE ON UPDATE CASCADE,
         PRIMARY KEY (email, org_code)
     );
 
     CREATE TABLE IF NOT EXISTS organizations (
         org_name VARCHAR(255) NOT NULL,
-        org_code CITEXT(4) NOT NULL PRIMARY KEY,
+        org_code CITEXT NOT NULL PRIMARY KEY,
         email VARCHAR(255) NOT NULL UNIQUE,
         pw_hashed VARCHAR(72) NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS user_orgs (
+        email VARCHAR(255) NOT NULL REFERENCES users(email) ON DELETE CASCADE ON UPDATE CASCADE,
+        org_code CITEXT NOT NULL REFERENCES organizations(org_code) ON DELETE CASCADE ON UPDATE CASCADE,
+        PRIMARY KEY (email, org_code)
     );
 
     CREATE TABLE IF NOT EXISTS trips (
@@ -42,7 +47,7 @@ BEGIN
 
     CREATE TABLE IF NOT EXISTS events (
         id SERIAL PRIMARY KEY,
-        org_code CHAR(4) NOT NULL REFERENCES organizations(org_code) ON DELETE CASCADE ON UPDATE CASCADE,
+        org_code CITEXT NOT NULL REFERENCES organizations(org_code) ON DELETE CASCADE ON UPDATE CASCADE,
         event_name VARCHAR(255) NOT NULL,
         event_date TIMESTAMP NOT NULL,
         include_time BOOLEAN NOT NULL DEFAULT false
