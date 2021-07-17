32211b153d6bb007ef5c092505916c806110ff33
Nathan Tang <nathan.ly.tang@gmail.com>
2021-07-16 20:47:12 -0600

    Fix: Spread request body to new variable

--- controllers/organizations.ts
+++ controllers/organizations.ts
@@ -11,13 +11,12 @@ import { Success, Error } from "../models/return_status";
 export async function orgRegister(req: Request, res: Response): Promise<Response> {
     try {
         // Organization registration logic
-        const org: orgModel.Organization = req.body;
+        const org: orgModel.Organization = { ...req.body };
         if (await db.checkEntityExists(org.email, "organizations")) {
             return res.status(409).json(new Error("org already exists"));
         }
 
         org.password = await auth.hashPassword(org.password);
-
         if (!await services.orgService.createNewOrg(org)) {
             return res.status(500).json(new Error("failed to create org"));
         }
@@ -50,6 +49,7 @@ export async function login(req: Request, res: Response): Promise<Response> {
         // Return token
         res.cookie('ACCESS_TOKEN', access_token, { httpOnly: true, expires: new Date(new Date().getTime() + 1000 * parseInt(process.env.ACCESS_TOKEN_EXPIRY || "")), sameSite: "strict" });
         res.cookie('REFRESH_TOKEN', refresh_token, { httpOnly: true, expires: new Date(new Date().getTime() + 1000 * parseInt(process.env.REFRESH_TOKEN_EXPIRY || "")), path: "/token/refresh", sameSite: "strict" });
+        res.cookie('REFRESH_TOKEN_VALID', true, { expires: new Date(new Date().getTime() + 1000 * parseInt(process.env.REFRESH_TOKEN_EXPIRY || "")), sameSite: "strict" });
         return res.status(200).json(new Success({ "access_token": access_token, "refresh_token": refresh_token }));
     } catch (err) {
         console.error(err);
